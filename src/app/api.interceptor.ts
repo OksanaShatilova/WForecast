feb574c263a489b29603aa13bc4480f82b635c4d
MacBook <macbook@MacBook-Pro-MacBook.local>
2020-04-04 13:38:34 +0300

    add condition to api.interceptor

--- src/app/api.interceptor.ts
+++ src/app/api.interceptor.ts
@@ -4,12 +4,13 @@ import {tap} from 'rxjs/operators';
 
 export class ApiInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    const cloned = req.clone({
-      params: req.params.append('key', '1b2e41cc6ea548908cef9ea8499e4875')
-    })
-
-    console.log(cloned);
-    return next.handle(cloned);
+    if (req.url.includes('api.weatherbit.io/v2.0/forecast')) {
+      const cloned = req.clone({
+        params: req.params.append('key', '1b2e41cc6ea548908cef9ea8499e4875')
+      });
+      return next.handle(cloned);
+    } else {
+      return next.handle(req);
+    }
   }
 }

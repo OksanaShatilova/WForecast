27bf2d3cb186b82f0b00888fb50825a261dd6242
MacBook <macbook@MacBook-Pro-MacBook.local>
2020-04-09 04:35:45 +0300

    remove unused

--- src/app/api.interceptor.ts
+++ src/app/api.interceptor.ts
@@ -1,6 +1,5 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
 
 export class ApiInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {

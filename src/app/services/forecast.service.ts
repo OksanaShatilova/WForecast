3adabe2fe08221087d5d92bd9881747511f1f16e
MacBook <macbook@MacBook-Pro-MacBook.local>
2020-04-10 19:10:49 +0300

    use HttpParams instead of template string

--- src/app/services/forecast.service.ts
+++ src/app/services/forecast.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 export interface DataItem {
@@ -21,6 +21,9 @@ export class ForecastService {
   constructor(private http: HttpClient) {
   }
   getForecast(city: string, days: number): Observable<any> {
-    return this.http.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&days=${days}`);
+    const params = new HttpParams()
+      .set('city', city)
+      .set('days', days.toString());
+    return this.http.get('https://api.weatherbit.io/v2.0/forecast/daily', {params});
   }
 }
